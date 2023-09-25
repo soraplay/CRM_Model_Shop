18c6e99c384f4468e3af262618a5e92b55d914e2
sorap_qfyxxb1 <sorap_qfyxxb1@DESKTOP-A1P7VQH>
2023-09-25 21:05:41 +0300

    Добавьте файлы проекта.

---
+++ CRM_Bl/Model/Check.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CRM_Bl.Model
+{
+    class Check
+    {
+        public int CheckId { get; set; }
+        public int CustomerId { get; set; }
+        public virtual Customer Customer { get; set; }
+        public int SellerId { get; set; }
+        public virtual Seller Seller { get; set; }
+        public DateTime Created { get; set; }
+        public override string ToString()
+        {
+            return $"№ {CheckId} от {Created.ToString("dd.MM.yy hh:mm:ss")}";
+        }
+    }
+}
---
+++ CRM_Bl/Model/Seller.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace CRM_Bl.Model
+{
+    class Seller
+    {
+        public int SellerId { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Check> Checks { get; set; }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
